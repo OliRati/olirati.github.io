b135f72ca2f213c4d741b829d71411c2e19c52b2
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2024-11-25 20:46:51 +0100

    Enhancement: Wait for all images to be loaded before animation start

--- assets/js/opening_script.js
+++ assets/js/opening_script.js
@@ -6,3 +6,42 @@ function exitAnimation() {
         document.location = "./index2.html";
     }, 1000);
 }
+
+// wWit for all images on a page to load before starting animation
+document.addEventListener("DOMContentLoaded", function () {
+    let images = document.querySelectorAll("img");
+    let totalImages = images.length;
+    let imagesLoaded = 0;
+
+    images.forEach(function (img) {
+        if (img.complete) {
+            incrementCounter();
+        } else {
+            img.addEventListener("load", incrementCounter);
+            img.addEventListener("error", incrementCounter); // In case of an error, still increment the counter
+        }
+    });
+
+    function incrementCounter() {
+        imagesLoaded++;
+        
+        const progress = document.getElementById("progress");
+        progress.innerText += ".";
+
+        if (imagesLoaded === totalImages) {
+            allImagesLoaded();
+        }
+    }
+
+    function allImagesLoaded() {
+        // All images have been loaded
+
+        // Make animation visible
+        let loading = document.getElementById("loading");
+        loading.style.display = "none";
+        let playing = document.getElementById("playing");
+        playing.style.display = "block";
+
+        // Make animation start
+    }
+});
